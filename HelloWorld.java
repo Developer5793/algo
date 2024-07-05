6aaec6784b5464b9baf8a3d7e6107e4da7d7f0cf
Clemens Osbahr <110489826+Developer5793@users.noreply.github.com>
2024-07-05 11:43:35 +0200

    Add files via upload
    Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+//Clemens Osbahr
+class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+        String name1 = "Clemens";
+        String name2 = "Osbahr";
+        System.out.println("Hello " + name1 + " " + name2);
+    }
+}
